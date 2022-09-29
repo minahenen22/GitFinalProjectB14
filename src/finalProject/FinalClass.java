68bece58d52cc497ea4cd23ad31980a4c0a6f4c9
minahenen <w.minahanna@gmail.com>
2022-09-28 20:11:12 -0400

    first commit

---
+++ src/finalProject/FinalClass.java
@@ -0,0 +1,12 @@
+package finalProject;
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is the final code for new car");
+
+	}
+
+}
